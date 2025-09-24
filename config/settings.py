93ac8a55639c63a71a72555c6027474887927b27
davidblue90125 <davidblue40@hotmail.com>
2025-09-24 11:55:16 +0100

    amend allowed_hosts

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-x$qnp2@^#d41g*a-kd3+q$o^smv9!1-5l#*vkj5)(br!mm17va
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.herokuapp.com',]
+ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1', 'localhost']
 
 
 # Application definition
