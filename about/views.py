12252dd44a715b01cf3f5f1e1b73a8d09f7fe9b1
davidblue90125 <davidblue40@hotmail.com>
2025-09-29 15:06:14 +0100

    add code to comtact form

--- about/views.py
+++ about/views.py
@@ -1,15 +1,27 @@
 from django.shortcuts import render
+from django.contrib import messages
 from .models import About
+from .forms import ContactForm
 
 # Create your views here.
 def about_me(request):
     """
     Renders the About page
     """
+    if request.method == "POST":
+        contact_form = ContactForm(data=request.POST)
+        if contact_form.is_valid():
+            contact_form.save()
+            messages.add_message(
+                request, messages.SUCCESS,
+                'Thanks for your message! I will get back to you shortly.'
+            )
     about = About.objects.all().order_by('-updated_on').first()
+    contact_form = ContactForm()
 
     return render(
         request,
         "about/about.html",
-        {"about": about},
+        {"about": about,
+        "contact_form": contact_form}
     )
